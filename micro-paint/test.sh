cfe2155c87c89cda375e64eed4567961cba4d31d
Jaehwan Kim <jaehwkim@c2r4s1.42seoul.kr>
2022-06-20 12:01:34 +0900

    exam03

--- micro-paint/test.sh
+++ micro-paint/test.sh
@@ -1,40 +1,40 @@
-gcc micro.c -o nooriginal -Wall -Wextra -Werror
+gcc micro.c -o a.out -Wall -Wextra -Werror
 gcc our_micro_paint.c -o original -Wall -Wextra -Werror
 echo ""
 echo "TEST - <ex_1>"
 echo "NO original"
-./nooriginal ./test/ex_1
+./a.out ./test/ex_1
 echo "Original"
 ./original ./test/ex_1
 echo ""
 echo "TEST - <ex_2>"
 echo "NO original"
-./nooriginal ./test/ex_2
+./a.out ./test/ex_2
 echo "Original"
 ./original ./test/ex_2
 echo ""
 echo "TEST - <ex_3>"
 echo "NO original"
-./nooriginal ./test/ex_3
+./a.out ./test/ex_3
 echo "Original"
 ./original ./test/ex_3
 echo ""
 echo "TEST - <ex_4>"
 echo "NO original"
-./nooriginal ./test/ex_4
+./a.out ./test/ex_4
 echo "Original"
 ./original ./test/ex_4
 echo ""
 echo "TEST - <NULL>"
 echo "NO original"
-./nooriginal
+./a.out
 echo "Original"
 ./original
 echo ""
 echo "TEST - <Error file>"
 echo "NO original"
-./nooriginal ./test/ex
+./a.out ./test/ex
 echo "Original"
 ./original ./test/ex
-rm nooriginal
+rm a.out
 rm original
