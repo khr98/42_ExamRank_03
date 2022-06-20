cc9ac193ce770628e5f290edfecad1bdf15b278d
Jaehwan Kim <jaehwkim@c2r4s1.42seoul.kr>
2022-06-20 14:56:16 +0900

    exam03

--- micro-paint/test.sh
+++ micro-paint/test.sh
@@ -1,36 +1,36 @@
 gcc micro.c -o a.out -Wall -Wextra -Werror
-gcc our_micro_paint.c -o original -Wall -Wextra -Werror
-echo ""
+gcc criteria.c -o original -Wall -Wextra -Werror
+echo "-------------------------------------------------------"
 echo "TEST - <ex_1>"
 echo "NO original"
 ./a.out ./test/ex_1
 echo "Original"
 ./original ./test/ex_1
-echo ""
+echo "-------------------------------------------------------"
 echo "TEST - <ex_2>"
 echo "NO original"
 ./a.out ./test/ex_2
 echo "Original"
 ./original ./test/ex_2
-echo ""
+echo "-------------------------------------------------------"
 echo "TEST - <ex_3>"
 echo "NO original"
 ./a.out ./test/ex_3
 echo "Original"
 ./original ./test/ex_3
-echo ""
+echo "-------------------------------------------------------"
 echo "TEST - <ex_4>"
 echo "NO original"
 ./a.out ./test/ex_4
 echo "Original"
 ./original ./test/ex_4
-echo ""
+echo "-------------------------------------------------------"
 echo "TEST - <NULL>"
 echo "NO original"
 ./a.out
 echo "Original"
 ./original
-echo ""
+echo "-------------------------------------------------------"
 echo "TEST - <Error file>"
 echo "NO original"
 ./a.out ./test/ex
