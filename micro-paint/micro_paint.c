54f3f68abe0be4c905b5d3b798db83b329cf2550
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-07-08 10:47:43 +0900

    dw

---
+++ micro-paint/micro_paint.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ARG_ERR "Error: argument\n"
+#define FILE_ERR "Error: Operation file corrupted\n"
+int ret, bgW, bgH;
+char bgC, T, C;
+float X, Y, W, H;
+FILE *file;
+char **zone;
+
+int ft_putstr(char *str)
+{
+	for(int i = 0; str[i] != 0; i++)
+	{
+		write(1, &str[i], 1);
+	}
+	return 1;
+}
+
+int check(int x, int y)
+{
+	if(x < X || y < Y || (X+W) < x || (Y+H) < y)
+		return 0;
+	else if(x - X < 1 || y - Y < 1 || (X + W) - x < 1 || (Y + H) -y < 1)
+		return 1;
+	return 2;
+}
+
+int main (int ac,char **av)
+{
+	if(ac != 2)
+		return(ft_putstr(ARG_ERR));
+	file = fopen(av[1], "r");
+	if(file == NULL)
+		return (ft_putstr(FILE_ERR));
+	ret = fscanf(file, "%d %d %c\n", &bgW, &bgH, &bgC);
+	if(ret != 3 || bgW < 1 || bgW > 300 || bgH < 1 || bgH > 300)
+		return (ft_putstr(FILE_ERR));
+	zone = malloc(bgH * sizeof(char *));
+	for(int i = 0; i < bgH; i++)
+	{
+		zone[i] = malloc(bgW * sizeof(char));
+		memset(zone[i], bgC, bgW);
+		zone[i][bgW] = '\0';
+	}
+	while((ret = fscanf(file, "%c %f %f %f %f %c\n", &T, &X, &Y, &W, &H, &C)) == 6)
+	{
+		if((T != 'r' && T != 'R') || W < 1 || H < 1)
+			return(ft_putstr(FILE_ERR));
+		for(int y = 0; y < bgH; y++)
+		{
+			for(int x = 0; x < bgW; x++)
+			{
+				int cond = check(x, y);
+				if((cond == 2 && T == 'R') || cond == 1)
+					zone[y][x] = C;
+			}
+		}
+	}
+	if(ret != -1)
+		return (ft_putstr(FILE_ERR));
+	for(int i = 0; i < bgH; i++)
+	{
+		ft_putstr(zone[i]);
+		ft_putstr("\n");
+	}
+	return 0;
+}
